092e033938f605075452be79239641d903fcc6e5
Rafael Palomar <rafaelpalomaravalos@gmail.com>
2020-11-22 21:38:20 +0100

    Adding levels and icons to status battery

--- .local/share/status/battery.sh
+++ .local/share/status/battery.sh
@@ -5,5 +5,31 @@
 status=$(cat /sys/class/power_supply/BAT0/status)
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 
-if [ ${status} == "Discharging" ]; then echo "D-${capacity}%"; 
-elif [ ${status} == "Charging" ]; then echo "C-${capacity}%"; fi
+if [ ${status} == "Discharging" ]; then
+
+    if [ ${capacity} -le 5 ]; then
+        echo "   ${capacity}%";
+    elif [ ${capacity} -gt 5 ] && [ ${capacity} -le 25 ]; then
+        echo "   ${capacity}%";
+    elif [ ${capacity} -gt 25 ] && [ ${capacity} -le 50 ]; then
+        echo "   ${capacity}%";
+    elif [ ${capacity} -gt 50 ] && [ ${capacity} -le 75 ]; then
+        echo "   ${capacity}%";
+    elif [ ${capacity} -gt 75 ] && [ ${capacity} -le 100 ]; then
+        echo "   ${capacity}%";
+    fi
+
+elif [ ${status} == "Charging" ]; then
+
+    if [ ${capacity} -le 5 ]; then
+        echo "  ${capacity}%";
+    elif [ ${capacity} -gt 5 ] && [ ${capacity} -le 25 ]; then
+        echo "  ${capacity}%";
+    elif [ ${capacity} -gt 25 ] && [ ${capacity} -le 50 ]; then
+        echo "  ${capacity}%";
+    elif [ ${capacity} -gt 50 ] && [ ${capacity} -le 75 ]; then
+        echo "  ${capacity}%";
+    elif [ ${capacity} -gt 75 ] && [ ${capacity} -le 100 ]; then
+        echo "  ${capacity}%";
+    fi
+fi
