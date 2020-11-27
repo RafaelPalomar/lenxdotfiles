0e66f1b7cd9fe98b8456bdf7dbd3e5aa105f428e
Rafael Palomar <rafaelpalomaravalos@gmail.com>
2020-11-27 13:58:02 +0100

    Adding comments on requirements for remap.sh

--- .local/bin/remap.sh
+++ .local/bin/remap.sh
@@ -8,10 +8,13 @@
 # xset r rate 300 5
 
 # Map the caps lock key to super...
+# REQ: setxkbmap
 setxkbmap -option caps:super
 
 # But when it is pressed only once, treat it as escape.
+# REQ: xcape
 killall xcape 2>/dev/null ; xcape -e 'Super_L=Escape'
 
 # Map the menu button to right super as well.
+# REQ: xmodmap
 xmodmap -e 'keycode 135 = Super_R'
